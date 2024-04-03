e9671d78b1bb917752fd08437c3018737e63116e
Jay C <jakecen@gmail.com>
2024-04-03 02:57:53 -0400

    CardSet main

--- forgetcurve_react/src/App.js
+++ forgetcurve_react/src/App.js
@@ -1,10 +1,12 @@
-
-import Header from "./components/Header";
+import MainPage from "./components/MainPage";
+import Header from "./components/header/Header";
 
 function App() {
   return (
     <>
-      <Header></Header>
+      <Header />
+      <MainPage />
+      
     </>
   );
 }
---
+++ forgetcurve_react/src/components/MainPage.js
@@ -0,0 +1,12 @@
+import CardSet from "./CardSetMain";
+import SearchBar from "./header/SearchBar";
+
+const MainPage = () => {
+  return (
+  <>
+    <CardSet></CardSet>
+  </>
+  )
+};
+
+export default MainPage;
