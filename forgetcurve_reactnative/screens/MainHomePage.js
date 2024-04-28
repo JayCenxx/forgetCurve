e1ada2fb5df84a12732deb8042dd9ab87ed68dc8
samantha_nisus_johnson <128253624+SamJohn87@users.noreply.github.com>
2024-04-28 18:45:31 -0500

    added header to home screen

--- forgetcurve_reactnative/screens/MainHomePage.js
+++ forgetcurve_reactnative/screens/MainHomePage.js
@@ -1,10 +1,40 @@
-import { Text, View } from 'react-native';
+import { Text, View, Button } from 'react-native';
+import { Icon } from 'react-native-elements';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+function HomeScreen() {
+    return (
+        <View className="flex-1 items-center justify-center bg-white">
+            <Text>Home Screen</Text>
+        </View>
+    );
+}
 
 function MainHomePage() {
     return (
-        <View className="flex-1 items-center justify-center bg-white">
-            <Text>Main Home Page</Text>
-        </View>
+        <Stack.Navigator>
+            <Stack.Screen
+                name="Home"
+                component={HomeScreen}
+                options={{
+                    title: 'Unicard',
+                    headerTintColor: 'blue',
+                    headerRight: () => (
+                        <Icon
+                            name='search'
+                            type='feather'
+                            size={24}
+                            iconStyle={{ width: 24 }}
+                            color="blue"
+                            onPress={() => alert('Search set!')}
+                        />
+                    ),
+                }}
+            />
+        </Stack.Navigator>
     );
 }
 
